2a83c6dffe65bad1c4d424235b69e5d5ad25c3b8
Yash <theflashyash99@gmail.com>
2025-08-21 13:34:10 +0530

    Motion keyframes

--- Framer-Motion/src/App.jsx
+++ Framer-Motion/src/App.jsx
@@ -1,14 +1,53 @@
 import { motion } from "motion/react";
+import oggy from "./assets/pngwing.com.png";
 
 const App = () => {
   return (
     <div>
-      <motion.div
+      <>
+        {/* <motion.div
         initial={{ x: 300 }}
         animate={{ x: 1000, rotate: 360, scale: 2 }}
         transition={{ duration: 3, delay: 1, repeat:2 , ease:"anticipate"}}  
         className="box"
+      >Shivam Gulati</motion.div> */}
+
+
+
+
+        {/* <motion.div
+        animate={{ x: [0,500,500,0,0], y:[0,0,300,300,0] , rotate:[0,360,0,360,0]  }}
+        transition={{ duration: 3, delay: 1, repeat:Infinity , ease:"anticipate"}}  
+        className="box"
+      >Shivam Gulati</motion.div> */}
+         
+         //! whileHover
+       <motion.div 
+       whileHover={{background:"green", scale:2.3 , rotate:360}}
+       transition={{duration:5 , repeat:Infinity}}
+        className="box"
       ></motion.div>
+
+
+        {/* <motion.img
+          initial={{
+            x:100,
+            y:100
+          }}
+          animate={{ x: 500 , y:200 , rotate:360 , scale:2}}
+          transition={{ duration: 5, repeat: Infinity, ease: "anticipate" }}
+          src={oggy}
+        />
+
+        {/* <motion.p className="oggy" animate={{x:1000}} transition={{duration:5, repeat:Infinity , ease:"anticipate"}}>"Main hu Oggy!!! BHeem ke diwane"</motion.p> */}
+        {/* <motion.p
+          className="oggy"
+          animate={{ x: 1000 }}
+          transition={{ duration: 5, repeat: Infinity, ease: "anticipate" }}
+        >
+          "Shivam CHinar"
+        </motion.p> */} 
+      </>
     </div>
   );
 };
