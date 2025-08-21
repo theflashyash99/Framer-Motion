18b76556ab254751a27e365fb78da349661f6503
Yash <theflashyash99@gmail.com>
2025-08-21 11:22:31 +0530

    Motion ease prop

--- Framer-Motion/src/App.jsx
+++ Framer-Motion/src/App.jsx
@@ -1,14 +1,16 @@
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 
 const App = () => {
   return (
     <div>
-    <motion.div animate={{x:1000,rotate:360}} transition={{duration:3 , delay:1}} className='box'>
-
-    </motion.div>
+      <motion.div
+        initial={{ x: 300 }}
+        animate={{ x: 1000, rotate: 360, scale: 2 }}
+        transition={{ duration: 3, delay: 1, repeat:2 , ease:"anticipate"}}  
+        className="box"
+      ></motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
