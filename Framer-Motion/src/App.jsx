9fbad7732d2fe25b0426ee7f6fb59eb2ef4e02c2
Yash <theflashyash99@gmail.com>
2025-08-21 11:07:42 +0530

    Framer-Motion Basics

---
+++ Framer-Motion/src/App.jsx
@@ -0,0 +1,13 @@
+import {motion} from "motion/react";
+
+const App = () => {
+  return (
+    <div>
+    <motion.div animate={{x:1000,rotate:360}} transition={{duration:3 , delay:1}} className='box'>
+
+    </motion.div>
+    </div>
+  )
+}
+
+export default App
